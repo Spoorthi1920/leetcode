6b517fed644bdf191fd46020a3c762f1b12dc235
Spoorthi1920 <162294992+Spoorthi1920@users.noreply.github.com>
2025-06-27 23:22:47 +0530

    Time: 0 ms (100%), Space: 40.7 MB (35.72%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        // String s = Integer.toBinaryString(n);
+        // int p = s.length();
+        // for(int i=0;i<p/2;i++){
+        //     if(s.charAt(i)!=s.charAt(p-i-1)){
+        //         return false;
+
+        //     }
+        // }
+        return false;
+        
+    }
+}
